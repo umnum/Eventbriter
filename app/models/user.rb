972bea980bdfc1f8dd1db1038d4916744fc9b3f2
Michael Castanieto <mcastanieto@gmail.com>
2020-10-12 13:49:10 -0700

    added 'users' table and User model with user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+    validates :username, :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+    attr_reader :password
+
+    def self.find_by_credendtials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = self.generate_session_token
+        self.save
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.generate_session_token
+    end
+end
