b45d52cc3a5bbd7c6602af603b3b8306291c07a5
Michael Castanieto <mcastanieto@gmail.com>
2020-10-12 17:04:49 -0700

    added UsersController #create action with corresponding 'show' view

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,19 @@ class Api::UsersController < ApplicationController
         @users = User.all
         render :index
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 end
