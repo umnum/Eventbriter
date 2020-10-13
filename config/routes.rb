c3467d6f5ef710a62dfd9d666e265790d3882a60
Michael Castanieto <mcastanieto@gmail.com>
2020-10-12 17:44:09 -0700

    added api/session#create and api/session#destroy to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create]
-    resources :session, only: [:create, :destroy]
+    resource :session, only: [:create, :destroy]
   end
 end
