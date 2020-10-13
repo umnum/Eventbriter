41073cdd28f12982d57c8609fe5d85ffbbeb6727
Michael Castanieto <mcastanieto@gmail.com>
2020-10-13 16:29:26 -0700

    added frontend routing functionality

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import HeaderContainer from './header/header_container';
+import Home from './home/home'
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
-            <h1>FSProject</h1>
+            <HeaderContainer />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={LoginFormContainer} />
+                <Route exact path="/signup" component={SignupFormContainer} />
+            </Switch>
         </div>
     );
 };
