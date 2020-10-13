a057bdeb7befb48b9aba8b8e88b8f1cfcf7ebe14
Michael Castanieto <mcastanieto@gmail.com>
2020-10-13 16:17:43 -0700

    authenticity token handled by jquery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery except: :index 
+    skip_before_action :verify_authenticity_token 
     helper_method :current_user, :logged_in?
 
     def current_user
