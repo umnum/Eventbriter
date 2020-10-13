3d4b53508780f3af1c1adf3813bb541703d7440a
Michael Castanieto <mcastanieto@gmail.com>
2020-10-13 16:21:10 -0700

    update reducer for handling cleared session errors

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,12 +1,14 @@
-import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from '../actions/session_actions';
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS, CLEAR_SESSION_ERRORS } from '../actions/session_actions';
 
 const sessionErrorsReducer = (oldState = [], action) => {
     Object.freeze(oldState);
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
             return [];
+        case CLEAR_SESSION_ERRORS:
+            return [];
         case RECEIVE_SESSION_ERRORS:
-            return Object.values(action.errors);
+            return Object.values(action.errors.responseJSON);
         default:
             return oldState;
     }
