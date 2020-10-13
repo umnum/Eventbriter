12b11000f37ecdf5173fd799bfe3c97bc81a5519
Michael Castanieto <mcastanieto@gmail.com>
2020-10-12 21:34:20 -0700

    added action creators receiveCurrentUser and receiveErrors, added signup thunk action creator

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,27 @@
+import * as SessionAPIUtils from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = (user) => {
+    return ({ 
+        type: RECEIVE_CURRENT_USER,
+        user
+    });
+}
+
+export const receiveErrors = (errors) => {
+    return ({
+        type: RECEIVE_SESSION_ERRORS,
+        errors
+    });
+};
+
+export const signup = user => {
+    return (dispatch => {
+        return SessionAPIUtils.signup(user)
+            .then(user => dispatch(receiveCurrentUser(user)),
+                  errors => dispatch(receiveErrors(errors.responseJSON)));
+    });
+};
