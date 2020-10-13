f27e6addea4f64b9a75de75cdad751c3991f7b7f
Michael Castanieto <mcastanieto@gmail.com>
2020-10-12 21:57:30 -0700

    added a store to the fs_project.jsx entry file

--- frontend/fs_project.jsx
+++ frontend/fs_project.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    const store = configureStore();
     ReactDOM.render(<h1>React definitely not broken!</h1>, root);
 });
