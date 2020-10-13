dc4e3088380eafb198b035e237e5172e0048296d
Michael Castanieto <mcastanieto@gmail.com>
2020-10-13 10:27:46 -0700

    user login auth is done via email and password

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,14 +2,14 @@ class Api::SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(
-            params[:user][:username],
+            params[:user][:email],
             params[:user][:password]
         )
         if @user
             login!(@user)
             render :show
         else
-            render json: {errors: "Invalid username or password"}, status: 401
+            render json: {errors: "Invalid email or password"}, status: 401
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
     after_initialize :ensure_session_token
     attr_reader :password
 
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
         if user && user.is_password?(password)
             user
         else
