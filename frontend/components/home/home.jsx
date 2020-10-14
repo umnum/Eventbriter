7ab1a46fe74be2ceefb7122fe5be070a831cf13a
Michael Castanieto <mcastanieto@gmail.com>
2020-10-13 21:54:24 -0700

    added a class label to the 'Home Page' tag

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return(
-            <h1>Home Page</h1>
+            <h1 className="home">Home Page</h1>
         );
     };
 };
