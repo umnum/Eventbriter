f6080153a0968c1929e3a41e52a303702d12d88b
Michael Castanieto <mcastanieto@gmail.com>
2020-10-15 13:39:56 -0700

    signup/login form text fields highlight blue when in focus

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -8,10 +8,16 @@ class SessionForm extends React.Component {
         this.state = {
             email: '', 
             password: '',
-            username: ''
+            username: '',
+            design: {
+                email: false,
+                password: false,
+                username: false
+            },
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.clearErrors = this.clearErrors(this);
+        this.focus = this.focus.bind(this);
     }
 
     handleSubmit(e) {
@@ -26,6 +32,30 @@ class SessionForm extends React.Component {
         };
     }
 
+    focus(form) {
+        //document.getElementsByClassName('email-field-wrapper')[0].classList.add("new-class");
+        return (e) => {
+            //this.setState({[form]: e.target.value});
+            let email = false;
+            let username = false;
+            let password = false;
+            if (form === 'email') {
+                email = true;
+            }
+            else if (form === 'username') {
+                username = true;
+            }
+            else if (form === 'password') {
+                password = true;
+            }
+            this.setState({design: {email, username, password}});
+        };
+    }
+
+    blur() {
+        this.setState({design: false})
+    }
+
     clearErrors() {
         this.props.clearErrors();
     }
@@ -35,6 +65,9 @@ class SessionForm extends React.Component {
         const errors = this.props.errors.map((error, idx) => {
             return <p key={idx}>{error}</p>;
         });
+        let usernameBorder = this.state.design.username ? "new-class" : "";
+        let emailBorder = this.state.design.email ? "new-class" : "";
+        let passwordBorder = this.state.design.password? "new-class" : "";
         return (
             <div className="session-container">
                 <div className="session-form">
@@ -42,18 +75,24 @@ class SessionForm extends React.Component {
                             <Link onClick={this.clearErrors} to="/signup">sign up</Link> :
                             <Link onClick={this.clearErrors} to="/login">log in</Link>}</h1>
                     <form onSubmit={this.handleSubmit}>
-                        <div className="email-field-wrapper">
-                            <label>Email address</label>
-                            <input onChange={this.handleInput('email')} type="text" value={this.state.email}/>
+                        <div className={'field-wrapper'}>
+                            <div className={`email-field-wrapper ${emailBorder}`}>
+                                <label id="email-label">Email address</label>
+                                <input onBlur={this.blur.bind(this)} onFocus={this.focus('email')} onChange={this.handleInput('email')} type="text" value={this.state.email}/>
+                            </div>
                         </div>
                         {isLogin ? <></> : 
-                        <div className="username-field-wrapper">
-                            <label>Full Name</label>
-                            <input onChange={this.handleInput('username')} type="text" value={this.state.username}/>
+                        <div className="field-wrapper">
+                            <div className={`username-field-wrapper ${usernameBorder}`}>
+                                <label id="username-label">Full Name</label>
+                                <input onBlur={this.blur.bind(this)} onFocus={this.focus('username')} onChange={this.handleInput('username')} type="text" value={this.state.username}/>
+                            </div>
                         </div>}
-                        <div className="password-field-wrapper">
-                            <label>Password</label>
-                            <input onChange={this.handleInput('password')} type="password" value={this.state.password}/>
+                        <div className="field-wrapper">
+                            <div className={`password-field-wrapper ${passwordBorder}`}>
+                                <label id="password-label">Password</label>
+                                <input onBlur={this.blur.bind(this)} onFocus={this.focus('password')} onChange={this.handleInput('password')} type="password" value={this.state.password}/>
+                            </div>
                         </div>
                         <button type="submit">{isLogin ? "Log In" : "Sign Up"}</button>
                     </form>
