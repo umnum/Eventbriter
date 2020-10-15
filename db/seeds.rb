0208b3dcf8c1d9aab73af262f33a36ccad7fcfbd
Michael Castanieto <mcastanieto@gmail.com>
2020-10-15 15:35:34 -0700

    seeded my database with 5 users, including a demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user1 = User.create!(username: 'Demo User', email: 'demo@mail.com', password: 'passworddemo')
+user2 = User.create!(username: 'Steve Woz', email: 'swoz@mail.com', password: 'passwordsw')
+user3 = User.create!(username: 'Sarah Connor', email: 'sconner@mail.com', password: 'passwordsc')
+user4 = User.create!(username: 'Michael Jordan', email: 'mjordan@mail.com', password: 'passwordmj')
+user5 = User.create!(username: 'Dave Chappelle', email: 'dchappelle@mail.com', password: 'passworddc')
