f58348304d5fda5e2e53af332986e8e2675c0f65
Michael Castanieto <mcastanieto@gmail.com>
2020-10-15 23:19:31 -0700

    added backend routes for events and categories controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#root"
+  resources :categories, only: [:create, :destroy]
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create]
     resource :session, only: [:create, :destroy]
+    resources :events, only: [:index, :show, :create, :update, :destroy]
+    resources :categories, only: [:index, :show]
   end
 end
