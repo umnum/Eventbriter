37f42a1fdda5ae9f0af3ebc377ad5c37c69bb460
Michael Castanieto <mcastanieto@gmail.com>
2020-10-15 23:17:16 -0700

    added Event model

---
+++ app/models/event.rb
@@ -0,0 +1,15 @@
+class Event < ApplicationRecord
+    validates :name, :location, :event_type, :description, presence: true
+    validates :start_date, :end_date, :timezone, :capacity, presence: true
+    validates :status, :start_sales_date, presence: true
+
+    belongs_to :organizer,
+        primary_key: :id,
+        foreign_key: :organizer_id,
+        class_name: :User
+
+    belongs_to :category,
+        primary_key: :id,
+        foreign_key: :category_id,
+        class_name: :Category
+end
