b8fa4d74fde523d00a72543ef61fdbdadbe8892f
Michael Castanieto <mcastanieto@gmail.com>
2020-10-18 00:11:58 -0700

    created an events reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import eventsReducer from './events_reducer';
+import categoriesReducer from './categories_reducer'
 
 const entitiesReducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    events: eventsReducer,
+    categories: categoriesReducer
 });
 
 export default entitiesReducer;
---
+++ frontend/reducers/events_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_EVENT, RECEIVE_EVENTS, REMOVE_EVENT } from '../actions/event_actions';
+
+const eventsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    let newState = Object.assign({}, oldState);
+    switch(action.type) {
+        case RECEIVE_EVENTS:
+            return action.events;
+        case RECEIVE_EVENT:
+            newState[action.event.id] = action.event;
+            return newState;
+        case REMOVE_EVENT:
+            delete newState[action.eventId];
+            return newState;
+        default:
+            return oldState;
+    }
+}
+
+export default eventsReducer;
