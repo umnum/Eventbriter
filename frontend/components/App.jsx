53f68a4124b298b043cb857aa3bda3a9559c6f79
Michael Castanieto <mcastanieto@gmail.com>
2020-10-18 00:18:15 -0700

    added routes for home page and event update form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import HeaderContainer from './header/header_container';
-import Home from './home/home'
+import HomeContainer from './home/home_container'
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import EditEventFormContainer from './events/edit_event_form_container';
 import { Switch, Route } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
 import NotFound from './not_found';
@@ -12,9 +13,10 @@ const App = () => {
         <div>
             <HeaderContainer />
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={HomeContainer} />
                 <AuthRoute exact path="/login" component={LoginFormContainer} />
                 <AuthRoute exact path="/signup" component={SignupFormContainer} />
+                <Route exact path="/events/:eventId/edit" component={EditEventFormContainer} />
                 <Route path="/" component={NotFound} />
             </Switch>
         </div>
