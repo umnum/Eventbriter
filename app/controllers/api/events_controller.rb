8c52aaf3202a03d904c5ca3e373c8c6fd61e5588
Michael Castanieto <mcastanieto@gmail.com>
2020-10-18 00:06:25 -0700

    created events controller with #index #show #create #update and #destroy actions

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -14,18 +14,38 @@ class Api::EventsController < ApplicationController
     end
 
     def create
+        @event = Event.new(event_params)
+        if @event.save
+            render :show
+        else
+            render json: @event.errors.full_messages, status: 422
+        end
     end
 
     def update
+        @event = Event.find_by(id: params[:id])
+
+        if @event.update(event_params)
+            render :show
+        else
+            render json: @event.errors.full_messages, status: 422
+        end
     end
 
     def destroy
+        @event = Event.find_by(id: params[:id])
+
+        if @event.destroy
+            render :show
+        else
+            render json: @event.errors.full_messages, status: 422
+        end
     end
 
     private
         def event_params
             params.require(:event).permit(:name, :location, :event_type,
                 :description, :start_date, :end_date, :timezone, :capacity,
-                :status, :start_sales_date, :organizer_id, :category_id)
+                :status, :start_sales_date, :organizer_id, :category_id, :event_image)
         end
 end
