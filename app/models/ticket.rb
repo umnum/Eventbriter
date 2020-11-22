36c7e8cc0959c6f291bca671ae240cee514b6415
Michael Castanieto <mcastanieto@gmail.com>
2020-11-22 14:55:24 -0800

    add model validations and associations to tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,13 @@
 class Ticket < ApplicationRecord
+    validates: :price, :currency, :quantity, presence: true
+
+    belongs_to user,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
+    belongs_to event,
+        primary_key: :id,
+        foreign_key: :event_id,
+        class_name: :Event
 end
