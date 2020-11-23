4dafd00019a1c25462d9e1c8821f55e9d0a79568
Michael Castanieto <mcastanieto@gmail.com>
2020-11-23 14:18:11 -0800

    add ticket thunk action creators

--- frontend/actions/ticket_actions.js
+++ frontend/actions/ticket_actions.js
@@ -1,3 +1,5 @@
+import * as TicketAPIUtils from '../util/ticket_api_util';
+
 export const RECEIVE_TICKET = 'RECEIVE_TICKET';
 export const RECEIVE_TICKETS = 'RECEIVE_TICKETS';
 export const REMOVE_TICKET = 'REMOVE_TICKET';
@@ -35,5 +37,45 @@ export const receiveTicketErrors = (errors) => {
 export const clearTicketErrors = () => {
     return ({
         type: CLEAR_TICKET_ERRORS
+    });
+};
+
+export const fetchTicket = ticketId => {
+    return (dispatch => {
+        return TicketAPIUtils.fetchTicket(ticketId)
+            .then(ticket => dispatch(receiveTicket(ticket)),
+                errors => dispatch(receiveTicketErrors(errors)))
+    });
+};
+
+export const fetchTickets = userId => {
+    return (dispatch => {
+        return TicketAPIUtils.fetchTickets(userId)
+            then(tickets => dispatch(receiveTickets(tickets)),
+                errors => dispatch(receiveTicketErrors(errors)))
+    });
+};
+
+export const createTicket = ticket => {
+    return (dispatch => {
+        return TicketAPIUtils.createTicket(ticket)
+            .then(ticket => dispatch(receiveTicket(ticket)),
+                errors => dispatch(receiveTicketErrors(errors)));
+    });
+};
+
+export const updateTicket = ticket => {
+    return (dispatch => {
+        return TicketAPIUtils.updateTicket(ticket)
+            .then(ticket => dispatch(receiveTicket(ticket)),
+                errors => dispatch(receiveTicketErrors(errors)));
+    });
+};
+
+export const removeTicket = ticketId => {
+    return (dispatch => {
+        return TicketAPIUtils.removeTicket(ticketId)
+            .then(ticket => dispatch(deleteTicket(ticketId)),
+                errors => dispatch(receiveTicketErrors(errors)));
     });
 };
