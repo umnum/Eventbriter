a27ec7ec2a03a384d71762ce497eaa3f88424f02
Michael Castanieto <mcastanieto@gmail.com>
2020-12-01 10:30:09 -0800

    add RECEIVE_EVENT action in tickets reducer

--- frontend/reducers/tickets_reducer.js
+++ frontend/reducers/tickets_reducer.js
@@ -1,5 +1,6 @@
 import { RECEIVE_TICKET, RECEIVE_TICKETS, REMOVE_TICKET } from '../actions/ticket_actions';
 import { RECEIVE_PURCHASED_TICKET, RECEIVE_PURCHASED_TICKETS } from '../actions/purchased_ticket_actions';
+import { RECEIVE_EVENT } from '../actions/event_actions';
 
 const ticketsReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
@@ -24,6 +25,11 @@ const ticketsReducer = (oldState = {}, action) => {
             newState[action.payload.ticket.id] = action.payload.ticket;
         case REMOVE_TICKET:
             delete newState[action.ticketId];
+            return newState;
+        case RECEIVE_EVENT:
+            Object.values(action.payload.tickets).forEach(ticket => {
+                newState[ticket.id] = ticket;
+            });
             return newState;
         default:
             return oldState;
